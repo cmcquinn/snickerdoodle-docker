95f89da7ee25282c26e82341a889d05eb4b47619
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-09-16 17:53:45 -0600

    rootfs: switch from debootstrap to multistrap

--- rootfs/init.sh
+++ rootfs/init.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# install debootstrap and qemu-user-static
+# install multistrap and qemu-user-static
 apt update
-apt install -y debootstrap qemu-user-static
+apt install -y multistrap qemu-user-static
 
 # mark image as prepared
 touch /etc/system-prepared

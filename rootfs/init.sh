54e40d8f1bc1f3a7859107e62c9de4134039fb4c
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-10-17 15:47:47 -0600

    rootfs: add Linaro GCC

--- rootfs/init.sh
+++ rootfs/init.sh
@@ -4,5 +4,11 @@
 apt update
 apt install -y multistrap qemu-user-static
 
+# download and install Linaro GCC
+cd /tmp
+curl -Lo linaro-gcc.tar.xz https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf.tar.xz
+cd gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf
+rsync -avz ./* /usr/local
+
 # mark image as prepared
 touch /etc/system-prepared

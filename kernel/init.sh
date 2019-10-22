73a08e9c441ccd8885cc3af32232110afed60d14
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-10-21 18:25:37 -0600

    Add rsync

--- kernel/init.sh
+++ kernel/init.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+set -e # exit on error
+set -x # echo commands
+
 # install multistrap and qemu-user-static
 apt update
-apt install -y multistrap qemu-user-static python3 libparted libparted-dev python3-pip
+apt install -y multistrap qemu-user-static python3 libparted libparted-dev python3-pip curl rsync
 
 # download and install Linaro GCC
 cd /tmp
 curl -Lo linaro-gcc.tar.xz https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf.tar.xz
+tar xf linaro-gcc.tar.xz
 cd gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf
 rsync -avz ./* /usr/local
 
--- rootfs/init.sh
+++ rootfs/init.sh
@@ -5,7 +5,7 @@ set -x # echo commands
 
 # install multistrap and qemu-user-static
 apt update
-apt install -y multistrap qemu-user-static python3 libparted libparted-dev python3-pip curl
+apt install -y multistrap qemu-user-static python3 libparted libparted-dev python3-pip curl rsync
 
 # download and install Linaro GCC
 cd /tmp

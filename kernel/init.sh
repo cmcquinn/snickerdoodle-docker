08aa33d1445a5dcabf9ca0272a1b71349385dab9
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-12-16 16:05:34 -0700

    Update kernel container to latest Linaro GCC, remove unneeded packages

--- kernel/init.sh
+++ kernel/init.sh
@@ -3,16 +3,22 @@
 set -e # exit on error
 set -x # echo commands
 
-# install multistrap and qemu-user-static
+# install curl and rsync
 apt update
-apt install -y multistrap qemu-user-static python3 libparted libparted-dev python3-pip curl rsync
+apt install -y curl rsync
 
 # download and install Linaro GCC
 cd /tmp
-curl -Lo linaro-gcc.tar.xz https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf.tar.xz
+curl -Lo linaro-gcc.tar.xz https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
 tar xf linaro-gcc.tar.xz
-cd gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf
+rm linaro-gcc.tar.xz
+cd $(find . -name gcc-linaro*)
 rsync -az ./* /usr/local
+
+# clean up temporary files to reduce image size
+cd /tmp
+rm -rf ./*
+apt autoclean
 
 # mark image as prepared
 touch /etc/system-prepared

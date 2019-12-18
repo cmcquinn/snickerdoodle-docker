b22d7fe3b6f205687d6efe7aefb39a62ab1af204
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-12-18 00:54:40 -0700

    Use arm-linux-gnueabihf toolchain from Debian repository

--- rootfs/init.sh
+++ rootfs/init.sh
@@ -7,13 +7,14 @@ set -x # echo commands
 apt update
 apt install --no-install-recommends -y multistrap qemu-user-static python3 libparted libparted-dev python3-pip curl rsync gnupg xz-utils
 
-# download and install Linaro GCC
-cd /tmp
-curl -Lo linaro-gcc.tar.xz https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
-tar xf linaro-gcc.tar.xz
-rm linaro-gcc.tar.xz
-cd $(find . -name gcc-linaro*)
-rsync -az ./* /usr/local
+# # download and install Linaro GCC
+# cd /tmp
+# curl -Lo linaro-gcc.tar.xz https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
+# tar xf linaro-gcc.tar.xz
+# rm linaro-gcc.tar.xz
+# cd $(find . -name gcc-linaro*)
+# rsync -az ./* /usr/local
+apt install -y gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf gcc-multilib
 
 # install Qt build dependencies
 apt install --no-install-recommends -y build-essential perl python3 git pkg-config

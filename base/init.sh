5590c1ae2e8d0bd28d19c41fde4e8362631100d6
Cameron McQuinn <cameron.mcquinn@gmail.com>
2020-01-16 18:32:06 -0700

    Use gcc-arm-linux-gnueabihf from Debian repos

--- base/init.sh
+++ base/init.sh
@@ -8,13 +8,8 @@ apt update
 apt install --no-install-recommends -y python3 python3-requests curl rsync gnupg xz-utils \
 ca-certificates build-essential git-buildpackage git pkg-config
 
-# download and install Linaro GCC
-cd /tmp
-curl -Lo linaro-gcc.tar.xz https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
-tar xf linaro-gcc.tar.xz
-rm linaro-gcc.tar.xz
-cd $(find . -name gcc-linaro*)
-rsync -az ./* /usr/local
+# install arm cross compiler
+apt install -y gcc-arm-linux-gnueabihf
 
 # install python utils
 cd /tmp
